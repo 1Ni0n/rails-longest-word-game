a2f252e925bff943330e61c364b8425d59187e6e
Anteo Guillot <anteo.guillot@gmail.com>
2017-02-13 17:38:04 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'run_game/grid'
-
-  get 'run_game/compute_score'
-
-  get 'run_game/score_and_message'
-
-  get 'run_game/get_translation'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
